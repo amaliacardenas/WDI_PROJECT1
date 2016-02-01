e91fdf633a567296568a00f981cf9bc7c5f0ca9c
Amalia Cardenas <amaliacardenas@gmail.com>
2016-02-01 16:49:00 +0000

    Added fonts, new background image,  styles on buttons

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
 
-  // $(".card").on("click", function() {
-  //   $(this).addClass("flipped")
-  // })
+  $(".card").on("mouseover", function() {
+    $(this).children().children().children().removeClass("hidden");
+  })
+
+  $(".card").on("mouseleave", function() {
+    $(this).children().children().children().addClass("hidden");
+  })
  
   var answers = ["Sister Mary Keneth Keller","Ada Lovelace",
 "Kimberly Bryant","Hedy Lamarr", "Zoe Quinn", "Rachel E. Walker", "Eliana Murillo","Jennifer Argüello","Tracy Chou", "Ruchi Sanghvi", "Grace Hoper",
@@ -32,8 +36,6 @@ $(document).ready(function(){
         $(this).parents('.card').addClass("flipped")
         console.log($(this));
 
-        // $(this).getElementById(box).classList.add("correct");
-        // document.getElementById(box).classList.remove("incorrect") 
       } else {
         console.log(answers[parentIndex]=== answer);
         turnCounter +=1;
@@ -62,15 +64,15 @@ function updateScoreBoard() {
 function getWinner(){
   if(player1 ===5){
 
-    $("#winner").text("player 1 won!");
+    $("#winner").text("Player 1 is the winner, congrats!");
     // alert("player 1 won!");
 
   } else if (player2 ===5){
-    $("#winner").text("player 2 won!");
+    $("#winner").text("Player 2 is the winner, congrats!");
     // alert("player 2 won!");
 
   } else {
-    $("#winner").text("Nobody won, sorry. Try again")
+    $("#winner").text("Keep playing, nobody has won yet.")
     // console.log("Nobody won. Try again.")
     }
   }
